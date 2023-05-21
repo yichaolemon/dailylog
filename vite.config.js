2c4a3008750ebdb06b47e487c3395d3df8d92247
yichaolemon <yclemontree0@gmail.com>
2023-05-21 16:08:24 -0700

    fix tag page

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,22 @@
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+
+const root = resolve(__dirname, 'src');
+const outDir = resolve(__dirname, 'dist');
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir,
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(root, 'index.html'),
+        tag: resolve(root, 'index.html'),
+      }
+    }
+  }
 })
